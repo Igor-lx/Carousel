3c82fe54bb01f691940d4d90b686a9ba84ceec8c
Igor-lx <lorax.k@yandex.ru>
2026-04-01 00:26:02 +0300

    fix

--- src/TEST_COMPONENT/components/index.ts
+++ src/TEST_COMPONENT/components/index.ts
@@ -1,5 +1,5 @@
 export * from "./slideItem";
 export * from "./pagination/pagination_basic/paginationBasic";
-export * from "./pagination/pagination_widget/PaginationWidget";
+export * from "./pagination/pagination_widget/paginationWidget";
 export * from "./pagination/pagination_widget/types";
 export * from "./controls";
